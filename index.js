9bc089148f3bbb2269363b665643d32a93e1e48d
raidoxd <raidol123@gmail.com>
2023-01-10 11:05:26 +0200

    .

--- index.js
+++ index.js
@@ -10,4 +10,30 @@ fetch(`${API_URL}?${query}`, options)
   .then(data => {
     console.log(data);
     // use the data here to create the chart
+    const userData = data.data.user;
+    // extract the labels and data values for the chart
+    const labels = userData.map(user => user.login);
+    const values = userData.map(user => user.id);
+    // create the chart
+    var ctx = document.getElementById('chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Value',
+                data: values,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
   });
