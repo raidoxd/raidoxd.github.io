d4728d4121e6ec733b2a70b429e19afcf4b2899e
raidoxd <raidol123@gmail.com>
2023-01-10 11:10:10 +0200

    .

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const API_URL = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
-const query = `query={user(where:{login:{_eq:"raidoxd"}}){id login}}`;
+const query = `{
+  user(where: { login: { _eq: "raidoxd" } }) {
+    id
+    login
+  }
+}`;
 
 const options = {
-  method: 'GET',
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query }),
 };
 
-fetch(`${API_URL}?${query}`, options)
+fetch(API_URL, options)
   .then(response => response.json())
   .then(data => {
     console.log(data);
+    
     // use the data here to create the chart
     const userData = data.data.user;
     // extract the labels and data values for the chart
