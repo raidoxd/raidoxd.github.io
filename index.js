a05b7923422a94a2ab805d24b7ac429eff91b839
raidoxd <raidol123@gmail.com>
2023-01-10 11:18:35 +0200

    .

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-const API_URL = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
 const query = `{
-  user(where: { login: { _eq: "raidoxd" } }) {
-    id
-    login
-  }
-}`;
-
-const options = {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ query }),
-};
-
-fetch(API_URL, options)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    
-    // use the data here to create the chart
-    const userData = data.data.user;
-    // extract the labels and data values for the chart
-    const labels = userData.map(user => user.login);
-    const values = userData.map(user => user.id);
-    // create the chart
-    var ctx = document.getElementById('chart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Value',
-                data: values,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
+    user(where: { login: { _eq: "raidoxd" } }) {
+      login
+      transactions{
+          amount
+      }
+    }
+  }`;
+  
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query }),
+  };
+  
+  fetch(API_URL, options)
+    .then(response => response.json())
+    .then(({ data }) => {
+      const transactions = data.user[0].transactions;
+      var ctx = document.getElementById('chart').getContext('2d');
+      var chart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: transactions.map(d => d.amount),
+              datasets: [{
+                  label: 'Transactions',
+                  data: transactions.map(d => d.amount),
+                  backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                  borderColor: 'rgba(255, 99, 132, 1)',
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      });
     });
-  });
+  
