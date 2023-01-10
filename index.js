f80a5806b39af3a2cecbb4f6e5f1897c27c5ce38
raidoxd <raidol123@gmail.com>
2023-01-10 11:02:06 +0200

    .

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const API_URL = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
-const query = `{
-  user(where: { login: { _eq: "raidoxd" } }) {
-    id
-    login
-  }
-}`;
+const query = `query={user(where:{login:{_eq:"raidoxd"}}){id login}}`;
 
 const options = {
   method: 'GET',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ query }),
 };
 
-fetch(API_URL, options)
+fetch(`${API_URL}?${query}`, options)
   .then(response => response.json())
   .then(data => {
     console.log(data);
     // use the data here to create the chart
   });
-
-
- 
