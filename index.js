b2eb529bf43df49d67af7dd4d59c06910643b4a6
raidoxd <raidol123@gmail.com>
2023-01-11 04:58:54 +0200

    .

--- index.js
+++ index.js
@@ -1,73 +1,71 @@
-
-
 const API_URL = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
-    const query = `
-        query transactionsByLogin($login: String!) {
-            user(where: { login: { _eq: $login } }) {
-              transactions(
-                  where: {type: {_eq: "xp"}, object: {type: {_nregex: "exercise|raid"}}}
-                  limit: 50
-                  order_by: {createdAt: asc}
-              ) {
-                amount
-                createdAt
-              }
-            }
+const query = `
+    query transactionsByLogin($login: String!) {
+        user(where: { login: { _eq: $login } }) {
+          transactions(
+              where: {type: {_eq: "xp"}, object: {type: {_nregex: "exercise|raid"}}}
+              limit: 50
+              order_by: {createdAt: asc}
+          ) {
+            amount
+            createdAt
+          }
         }
-    `;
-    const variables = {
-        login: "raidoxd"
     }
- 
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query, variables }),
-    };
+`;
+const variables = {
+    login: "raidoxd"
+};
 
-    fetch(API_URL, options)
-        .then(response => response.json())
-        .then(({ data }) => {
-            const transactions = data.user[0].transactions;
-            let runningTotal = 0;
-            const dataPoints = transactions.reduce((acc, cur) => {
-                runningTotal += cur.amount;
-                const date = new Date(cur.createdAt);
-                const formattedDate = date.toLocaleDateString();
-                acc.push({date: formattedDate, amount: runningTotal});
-                return acc;
-            }, []);
+const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query, variables }),
+};
 
-            const xScale = d3.scaleBand()
-                .domain(dataPoints.map(d => d.date))
-                .range([0, 750])
-                .padding(0.1);
+fetch(API_URL, options)
+    .then(response => response.json())
+    .then(({ data }) => {
+        const transactions = data.user[0].transactions;
+        let runningTotal = 0;
+        const dataPoints = transactions.reduce((acc, cur) => {
+            runningTotal += cur.amount;
+            const date = new Date(cur.createdAt);
+            const formattedDate = date.toLocaleDateString();
+            acc.push({date: formattedDate, amount: runningTotal});
+            return acc;
+        }, []);
 
-            const yScale = d3.scaleLinear()
-                .domain([0, d3.max(dataPoints, d => d.amount)])
-                .range([450, 50]);
+        const xScale = d3.scaleBand()
+            .domain(dataPoints.map(d => d.date))
+            .range([0, 750])
+            .padding(0.1);
 
-            const xAxis = d3.axisBottom(xScale);
-            const yAxis = d3.axisLeft(yScale);
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(dataPoints, d => d.amount)])
+            .range([450, 50]);
 
-            d3.select('#chart')
-                .append('g')
-                .attr('transform', 'translate(50, 0)')
-                .call(yAxis);
+        const xAxis = d3.axisBottom(xScale);
+        const yAxis = d3.axisLeft(yScale);
 
-            d3.select('#chart')
-                .append('g')
-                .attr('transform', 'translate(50, 450)')
-                .call(xAxis);
+        d3.select('#chart')
+            .append('g')
+            .attr('transform', 'translate(50, 0)')
+            .call(yAxis);
 
-            d3.select('#chart')
-                .selectAll('rect')
-                .data(dataPoints)
-                .enter()
-                .append('rect')
-                .attr('x', d => xScale(d.date))
-                .attr('y', d => yScale(d.amount))
-                .attr('width', xScale.bandwidth())
-                .attr('height', d => 450 - yScale(d.amount))
-                .attr('fill', 'blue');
-        });
+        d3.select('#chart')
+            .append('g')
+            .attr('transform', 'translate(50, 450)')
+            .call(xAxis);
+
+        d3.select('#chart')
+            .append("path")
+            .datum(dataPoints)
+            .attr("fill", "none")
+            .attr("stroke", "blue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+            .x(d => xScale(d.date))
+            .y(d => yScale(d.amount))
+            );
+    });
