3c04f14caa1d76da69626cc327fe986592d3cacb
raidoxd <raidol123@gmail.com>
2023-01-12 22:56:00 +0200

    .

--- index.js
+++ index.js
@@ -1,71 +1,60 @@
-const API_URL = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
-const query = `
-    query transactionsByLogin($login: String!) {
-        user(where: { login: { _eq: $login } }) {
-          transactions(
-              where: {type: {_eq: "xp"}, object: {type: {_nregex: "exercise|raid"}}}
-              limit: 50
-              order_by: {createdAt: asc}
-          ) {
-            amount
-            createdAt
-          }
-        }
-    }
-`;
-const variables = {
-    login: "raidoxd"
-};
 
+const API_URL = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
+const query = `{
+    user(where: { login: { _eq: "raidoxd" } }) {
+      transactions(
+          where: {type: {_eq: "xp"}, object: {type: {_nregex: "exercise|raid"}}}
+          limit: 50
+          order_by: {createdAt: asc}
+      ) {
+        amount
+        createdAt
+      }
+    }
+  }`;
 const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query, variables }),
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query }),
 };
 
 fetch(API_URL, options)
-    .then(response => response.json())
-    .then(({ data }) => {
-        const transactions = data.user[0].transactions;
-        let runningTotal = 0;
-        const dataPoints = transactions.reduce((acc, cur) => {
-            runningTotal += cur.amount;
-            const date = new Date(cur.createdAt);
-            const formattedDate = date.toLocaleDateString();
-            acc.push({date: formattedDate, amount: runningTotal});
-            return acc;
-        }, []);
-
-        const xScale = d3.scaleBand()
-            .domain(dataPoints.map(d => d.date))
-            .range([0, 750])
-            .padding(0.1);
-
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(dataPoints, d => d.amount)])
-            .range([450, 50]);
-
-        const xAxis = d3.axisBottom(xScale);
-        const yAxis = d3.axisLeft(yScale);
-
-        d3.select('#chart')
-            .append('g')
-            .attr('transform', 'translate(50, 0)')
-            .call(yAxis);
-
-        d3.select('#chart')
-            .append('g')
-            .attr('transform', 'translate(50, 450)')
-            .call(xAxis);
-
-        d3.select('#chart')
-            .append("path")
-            .datum(dataPoints)
-            .attr("fill", "none")
-            .attr("stroke", "blue")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-            .x(d => xScale(d.date))
-            .y(d => yScale(d.amount))
-            );
+  .then(response => response.json())
+  .then(({ data }) => {
+    console.log(data)
+    const transactions = data.user[0].transactions;
+    let runningTotal = 0;
+    const dataPoints = transactions.reduce((acc, cur) => {
+      runningTotal += cur.amount;
+      const date = new Date(cur.createdAt);
+      const formattedDate = date.toLocaleDateString();
+      acc.push([formattedDate, runningTotal]);
+      return acc;
+    }, []);
+    Highcharts.chart('chart', {
+      chart: {
+        type: 'line',
+       
+      },
+      title: {
+        text: 'Running total amount over time'
+      },
+      xAxis: {
+        type: 'category',
+        title: {
+          text: 'Date'
+        },
+        yAxis: {
+          title: {
+            text: 'Amount'
+          }
+        },
+        series: [{
+          name: 'Amount',
+          data: dataPoints
+        }]
+      }
     });
+  });
+
+
